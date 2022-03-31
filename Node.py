7b2e9f1fca7babac68f5105406b8100419878e4d
Ryan Kim <43964171+Ryan-JW-Kim@users.noreply.github.com>
2022-03-31 15:53:48 -0400

    Update Node.py
    Final addition, project complete with perfect output.

--- Node.py
+++ Node.py
@@ -1,6 +1,3 @@
-from itertools import count
-
-from matplotlib.font_manager import json_load
 from common import *
 
 class Node:
@@ -11,77 +8,114 @@ class Node:
         self.distanceTable = [[0 for i in range(num)] for j in range(num)]
         self.routes = [0 for i in range(num)]
 
+        self.has_link = [[] for i in range(num)]
+
+        # -----------------------INITALIZING DISTANCE TABLE---------------------
+        # For ever source in distanceTable
         for k in range(num):
+
+            # For each destination of the current source
             for j in range(num):
+
+                # If at row of the current node
                 if k == self.myID:
+                    # DistanceTable row is equals to the costs parameters
                     self.distanceTable[k] = costs
 
+
+                # If at the destination is equlas to source
                 elif k == j:
+                    # No connection (0)
                     self.distanceTable[k][j] = 0
 
+                # If not a self connection (0) or at the proper row for current source distanceTable at current [source][destination] connection is nonexistant (999)
                 else:
                     self.distanceTable[k][j] = self.ns.INFINITY
 
-        self.routes = self.distanceTable[self.myID]
+        # -------------------------INITALIZING ROUTES LIST---------------------------
 
-        source = self.myID
+        for dest in range(self.ns.NUM_NODES):
 
-        for destination in range(num):
-            if source != destination:
-                if costs[destination] != self.ns.INFINITY:
-                    pkt = RTPacket(source, destination, costs)
+            # If route exists
+            if self.distanceTable[self.myID][dest] != self.ns.INFINITY:
+                self.routes[dest] = dest
+                self.has_link[dest] = [dest, True]
 
-                    self.ns.tolayer2(pkt)
+            else:
+
+                self.has_link[dest] = [dest, False]
+
+        # -------------------------TRANSMITTING DISTANCE TABLE TO ALL NEIGHBOURS-----
+
+
+        for link in self.has_link:
+
+            router_id = link[0]
+            link_state = link[1]
+
+            if link_state is True and router_id != self.myID:
+                pkt = RTPacket(self.myID, router_id, costs)
+                self.ns.tolayer2(pkt)
+
+
+
 
     def recvUpdate(self, pkt):
 
         self.distanceTable[pkt.sourceid] = pkt.mincosts
 
-        # For every adjacent Node
-        for adj_I in range(self.ns.NUM_NODES):
+        # -------------------------BELLMAN FORD DISTANCE VECTOR ROUTING ALGORITHM--------------------------
 
-            # Exclude the current node receiving the update
-            if adj_I != self.myID:
+        retransmit = False
 
-                # Exclude non existant links (infinity)
-                if self.distanceTable[self.myID][adj_I] != self.ns.INFINITY:
 
-                    # Distance for source to the current adjacent router
-                    dist_source_adjacent = self.distanceTable[self.myID][adj_I]
+        # Assume router destinaion can be reached via router_id (updated costs from pkt.sourceid)
+        router_id = pkt.sourceid
 
-                    # For each possible end point of the current source
-                    for dest_I in range(self.ns.NUM_NODES):
+        # distance of link between this node and router_id (router with new min costs)
+        d1 = self.distanceTable[self.myID][router_id]
 
-                        # Exclude the router recieving update, and the router currently adjacent to source
-                        if dest_I != self.myID or dest_I != adj_I:
+        # for every destination router accessable via router
+        for router_destination_id in range(self.ns.NUM_NODES): # Excluding non-existant links
+            if self.distanceTable[router_id][router_destination_id] == self.ns.INFINITY:
+                pass
 
-                            # Exclude nonexistant links (infinity)
-                            if self.distanceTable[adj_I][dest_I] != self.ns.INFINITY:
 
-                                # Distance from source to destination
-                                dist_source_destination = self.distanceTable[self.myID][dest_I]
+            direct_cost = self.distanceTable[self.myID][router_destination_id]
 
-                                # Distance from current adjacent to destination
-                                dist_adjacent_destination = self.distanceTable[adj_I][dest_I]
+            # cost of link from router_id to router_destination_id
+            d3 = self.distanceTable[router_id][router_destination_id]
 
-                                # If there is a lower path from adjacent it becomes the new distance
-                                new_distance = min(dist_source_adjacent + dist_adjacent_destination, dist_source_destination)
-                                self.distanceTable[self.myID][dest_I] = new_distance
+            # Cost of path to router_destination_id via router
+            cost_via_router = d1 + d3
+
+            # If cost via router is less than direct cost
+            if cost_via_router < direct_cost and self.has_link[router_id][1]:
+
+                # set new distance as the cost via router
+                self.distanceTable[self.myID][router_destination_id] = cost_via_router
+                # Set the next hop for reaching router_destination_id to be via router
+
+                next_hop_to_router = self.routes[router_id]
+                self.routes[router_destination_id] = next_hop_to_router
+
+                # Since an alteration to this nodes distance table and next hop list was made, inform neighbours
+                retransmit = True
+
+        # -------------------------TRANSMIT NEW DISTANCETABLE TO ALL ADJACENT NODES--------------------------
+
+        # Do not retransmit if there were no new paths found
+        if retransmit:
         
-        # For every adjacent Node
-        for node in range(self.ns.NUM_NODES):
+            for link in self.has_link:
 
-            # Ignore the current node
-            if node != self.myID:
+                router_id = link[0]
+                link_state = link[1]
 
-                # Exclude nonexistant links (infinity)
-                if self.distanceTable[self.myID][node] != self.ns.INFINITY:
-
-                    # Create a packet
-                    pkt = RTPacket(self.myID, node, self.distanceTable[self.myID])
-
-                    # Send packet
+                if link_state is True and router_id != self.myID:
+                    pkt = RTPacket(self.myID, router_id, self.distanceTable[self.myID])
                     self.ns.tolayer2(pkt)
+
         return
 
     def printdt(self):
@@ -98,6 +132,7 @@ class Node:
             
             for j in range(self.ns.NUM_NODES):
                 print("{:3d}   ".format(self.distanceTable[i][j]), end="" )
+
+        
             print()            
         print()
-        
